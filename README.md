f44b0544b7a2e7f46eac27263d44bba8fca3c0fc
Learner <linhdqtg@gmail.com>
2019-01-28 23:30:07 +0700

    Fix target
    Swap codes

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Copyright (c) 2019 Bitcoin Mono Project.
 
 Copyright (c) 2018-2019 Swap developers. 
 
-Copyright (c) 2014-2018 The Monero Project.   
+Copyright (c) 2014-2019 The Monero Project.   
 
 Portions Copyright (c) 2012-2013 The Cryptonote developers.
 
--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -83,9 +83,9 @@ namespace cryptonote {
   }
   //-----------------------------------------------------------------------------------------------
   bool get_block_reward(size_t median_weight, size_t current_block_weight, uint64_t already_generated_coins, uint64_t &reward, uint8_t version) {
-    static_assert(DIFFICULTY_TARGET%60==0,"difficulty targets must be a multiple of 60");
+
     const int target = DIFFICULTY_TARGET;
-    const int target_minutes = target / 60;
+    const int target_minutes = 2; //genesis tx based on 120s
     const int emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1);
 
     uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
